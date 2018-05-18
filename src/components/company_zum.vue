e36294b98992aae7d540ea170fc0f37aa33f013e
Minhyuk Kim <minsoura@zuminternet.com>
2018-05-18 16:07:22 +0900

    contents added

--- src/components/company_zum.vue
+++ src/components/company_zum.vue
@@ -12,6 +12,7 @@
     <div class="company-title"><!-- news title-->
       투자
     </div>
+
     <div class="card-holder">
       <div class="row">
       <div class="col-md-3 col-md-4 col-md-2">
@@ -19,7 +20,9 @@
           <div class="issue" ><center>2017년 6월21일 14:42</center></div>
           <div class="card-body">
           <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/12/2018/05/08/7dc78c473fc94f159c1b3187ebb2aebe.jpg" alt="Card image cap">
+          <a href="http://news.zum.com/articles/38676996" style=" color:inherit">
             <h5 class="card-title"><b>줌인터넷, 쇼핑·콘텐츠 추천 앱 내며 모바일 사업 본격화</b></h5>
+          </a>
             <p class="card-text">줌인터넷이 모바일 사업을 강화한다. 김우승 줌인터넷 대표는 “이용자가 가장 많이 보는 뉴스 콘텐츠부터 우선 시작하지만 뉴스에 한정된 서비스는 아니다”면서 “이용자 피드백을 반영해 다양한 유형 정보를 추천하는 서비스로 고도화할 것”이라고 설명했다.</p>
           </div>
         </div>
@@ -48,31 +51,55 @@
       </div>
 
     </div>
-    </div>
+  </div>
   </div><!-- company-->
+
+
 
   <div class="company">
   <div class="company-title">
-    채용
+    금융
   </div>
   <div class="card-holder">
     <div class="row">
       <div class="col-md-3 col-md-4 col-md-2">
         <div class="card" style="width: 18rem;">
-          <div class="issue" ><center>2017년 10월11일 10:56</center></div>
+          <div class="issue" ><center>2018년 3월 06일 16:49</center></div>
           <div class="card-body">
-          <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/24/2017/10/11/0f01e2cd2e3b4a8eb6bdbfa8fffdb66f.jpg" alt="Card image cap">
-            <h5 class="card-title"><b>이스트소프트, ‘2017 하반기 공개 채용’… 학력·나이 제한 없어</b></h5>
-            <p class="card-text">종합 소프트웨어 기업 이스트소프트와 자회사 이스트시큐리티, 줌인터넷이 ‘2017년 하반기 공개 채용’을 진행한다. 10월 11일부터 24일까지 공식 홈페이지에서 온라인으로 서류 접수를 진행하며, 최대 40명 내외의 신입 및 경력 인력을 최종 선발할 계획이다. </p>
+          <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
+            <h5 class="card-title"><b>줌인터넷, 23일 주총…주식매수선택권 부여 한도 확대</b></h5>
+            <p class="card-text">줌인터넷은 오는 27일 오전 10시 서울시 서초구 줌인터넷 본사에서 제9기 정기주주총회를 연다고 6일 공시했다.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-md-4 col-md-2">
+        <div class="card" style="width: 18rem;">
+          <div class="issue" ><center>2017년 8월 14일 13:45</center></div>
+          <div class="card-body">
+          <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/12/2017/08/14/73559e1a080a4acaa82e3922ae12453d.jpg" alt="Card image cap">
+            <h5 class="card-title"><b>줌인터넷, 코스닥 상장예비심사 미승인…"대응안 마련해 재청구"</b></h5>
+            <p class="card-text">줌인터넷은 골든브릿지제3호스팩과 합병을 통한 코스닥 상장예비심사 결과 미승인 통보를 받았다고 14일 밝혔다. 줌인터넷이 업무 효율성 제고를 목적으로 모회사 이스트소프트와 영업조직을 통합하면서 모회사 의존성이 커졌다는 점이 미승인 사유로 지적받았다.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-3 col-md-4 col-md-2">
+        <div class="card" style="width: 18rem;">
+          <div class="issue" ><center>2016년 6월 10일 15:56</center></div>
+          <div class="card-body">
+          <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
+            <h5 class="card-title"><b>줌인터넷, 코넥스 상장… AI 기반 연구 통해 줌닷컴 서비스 성장 도모</b></h5>
+            <p class="card-text">개방형 검색포털 줌닷컴을 운영 중인 줌인터넷이 중소기업 전용주식시장인 코넥스에 10일 신규 상장했다. 회사 측 관계자는 “이번 코넥스 상장을 통해 지속적인 성장을 위한 발판을 마련했다”며, “실적증가에 발맞춰 인공지능 및 딥러닝 기술에 기반한 검색기술 연구개발에 대한 투자를 강화할 예정”이라고 전했다.</p>
           </div>
         </div>
       </div>
   </div><!-- row-->
-</div><!-- card-holder-->
+  </div><!-- card-holder-->
+  </div><!-- company-->
 
 
 
-</div><!-- company-->
 
 <div class="company">
 <div class="company-title">
@@ -117,33 +144,33 @@
 <div class="row">
   <div class="col-md-3 col-md-4 col-md-2">
     <div class="card" style="width: 18rem;">
-      <div class="issue" ><center>2018년 5월 8일 18:46</center></div>
+      <div class="issue" ><center>2017년 8월 23일 17:12</center></div>
       <div class="card-body">
-      <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/12/2018/05/08/7dc78c473fc94f159c1b3187ebb2aebe.jpg" alt="Card image cap">
-        <h5 class="card-title"><b>줌인터넷, 육아용품 리뷰 전문 앱 '리얼리뷰' 출시</b></h5>
-        <p class="card-text">줌인터넷은 육아용품 리뷰 전문 앱 '리얼리뷰' 안드로이드 버전을 출시했다. 리얼리뷰는 임신·출산·육아기 부모를 대상으로 출산과 육아 시기별 필요한 상품 정보를 사용후기와 함께 추천하는 리뷰 전문 앱이다.</p>
+      <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/12/2017/08/23/9eada18defe643a6839b3f926a602aa6.jpg" alt="Card image cap">
+        <h5 class="card-title"><b>줌인터넷, 블로그 '이글루스' iOS 모바일 앱 출시</b></h5>
+        <p class="card-text">줌인터넷은 자사 블로그 서비스 이글루스 iOS용 모바일앱을 출시, 공개 시범 서비스(OBT)를 실시한다고 23일 밝혔다. 6월 안드로이드 OS용 앱을 출시한 데 이어 이번에 iOS용 앱도 새롭게 선보였다.</p>
       </div>
     </div>
   </div>
 
   <div class="col-md-3 col-md-4 col-md-2">
     <div class="card" style="width: 18rem;">
-      <div class="issue" ><center>2017년 12월 28일 09:52</center></div>
+      <div class="issue" ><center>2017년 6월 21일 14:42</center></div>
       <div class="card-body">
-      <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/14/2017/12/28/9f13f7f1d4cf4ce68a49223b79425dfe.jpg" alt="Card image cap">
-        <h5 class="card-title"><b>펀드슈퍼마켓, 펀드정보 채널 확장…줌인터넷과 제휴</b></h5>
-        <p class="card-text">온라인 펀드판매채널 '펀드슈퍼마켓'을 운영하는 펀드온라인코리아는 줌인터넷과 제휴를 통해 줌닷컴에 '펀드정보 검색 서비스'를 오픈했다고 28일 밝혔다. 펀드정보 검색 서비스에선 펀드슈퍼마켓에서 제공하는 1600여개 펀드 상품을 검색하고 관련 정보를 확인할 수 있다.</p>
+      <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/12/2017/06/21/84bef25fbe7148908320b8196066e462.jpg" alt="Card image cap">
+        <h5 class="card-title"><b>줌인터넷, 쇼핑·콘텐츠 추천 앱 내며 모바일 사업 본격화</b></h5>
+        <p class="card-text">줌인터넷이 모바일 사업을 강화한다. 김우승 줌인터넷 대표는 “이용자가 가장 많이 보는 뉴스 콘텐츠부터 우선 시작하지만 뉴스에 한정된 서비스는 아니다”면서 “이용자 피드백을 반영해 다양한 유형 정보를 추천하는 서비스로 고도화할 것”이라고 설명했다.</p>
       </div>
     </div>
   </div>
 
   <div class="col-md-3 col-md-4 col-md-2">
     <div class="card" style="width: 18rem;">
-      <div class="issue" ><center>20171107 14:17</center></div>
+      <div class="issue" ><center>2017년 5월 29일 16:32</center></div>
       <div class="card-body">
-      <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/12/2017/11/07/907e64795b484cc985a8ad6dc44728cb.jpg" alt="Card image cap">
-        <h5 class="card-title"><b>줌인터넷, 여성 특화 쇼핑앱 '쇼즐' 출시...2000개 쇼핑몰 60만개 상품 제공</b></h5>
-        <p class="card-text">줌인터넷은 여성 쇼핑몰 특화 쇼핑앱 '쇼즐'을 출시했다고 7일 밝혔다. 쇼즐은 2000여개 여성 의류 쇼핑몰 중 사용자가 즐겨찾기 한 쇼핑몰 아이템을 모아 보여주는 안드로이드 OS용 쇼핑앱이다.</p>
+      <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/23/2017/05/29/edbf00a350e44314a134ce93ec99c2b7.jpg" alt="Card image cap">
+        <h5 class="card-title"><b>줌닷컴, 검색쿼리 점유율 2% 달성</b></h5>
+        <p class="card-text">29일 줌인터넷은 코리안클릭의 5월 3주 주간데이터 기준 통합 검색 쿼리 점유율이 2%를 달성했다고 밝혔다. 줌인터넷 측은 검색 점유율 확대가 서비스 활성화와 트래픽 증가로 이어지고, 광고 매출 확대에도 긍정적인 영향을 줄 것으로 예상하고 있다.</p>
       </div>
     </div>
   </div>
@@ -153,173 +180,57 @@
 
 <div class="company">
 <div class="company-title">
-  사고
+  수상
 </div>
 <div class="card-holder">
   <div class="row">
     <div class="col-md-3 col-md-4 col-md-2">
       <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
+        <div class="issue" ><center>2017년 12월 10일 12:00</center></div>
         <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/3/2017/12/10/AKR20171210015300017_01_i.jpg" alt="Card image cap">
+          <h5 class="card-title"><b>'가짜뉴스 판별 AI 개발대회' 이스트소프트·줌인터넷팀 우승</b></h5>
+          <p class="card-text">과학기술정보통신부는 가짜뉴스를 판별하는 기술을 겨루는 '인공지능(AI) R n D(연구개발) 챌린지'에서 ㈜이스트소프트와 줌인터넷㈜의 개발자들이 연합한 '디피스트팀'을 1위로 선정했다고 10일 밝혔다.</p>
         </div>
       </div>
     </div>
 
     <div class="col-md-3 col-md-4 col-md-2">
       <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
+        <div class="issue" ><center>2016년 12월 05일 21:04</center></div>
         <div class="card-body">
         <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 class="card-title"><b>서울고용노동청, 일家양득 우수기업 공모전 시상</b></h5>
+          <p class="card-text">서울고용노동청은 일과 가정의 양립을 통한 행복한 근로문화 정착을 위해 '일家양득 우수기업 공모전'을 개최하고 줌인터넷주식회사 등 총 6개 기업을 선정하여 시상했다. 최우수상을 수상한 줌인터넷주식회사는 지난 2009년 설립 당시부터 시차출퇴근제, 재택 근무제를 지속 운영, 현재는 약 50%의 직원이 이 제도를 활용해 일과 가정의 균형을 통한 삶의 질을 높이고 있다.</p>
         </div>
       </div>
     </div>
 
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-</div><!-- row-->
-</div><!-- card-holder-->
-</div><!-- company-->
-
-<div class="company">
-<div class="company-title">
-  조직이슈
-</div>
-<div class="card-holder">
-  <div class="row">
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-</div><!-- row-->
-</div><!-- card-holder-->
-</div><!-- company-->
-
-<div class="company">
-<div class="company-title">
-  금융
-</div>
-<div class="card-holder">
-  <div class="row">
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-</div><!-- row-->
-</div><!-- card-holder-->
-</div><!-- company-->
-
-<div class="company">
-<div class="company-title">
-  이익활동
-</div>
-<div class="card-holder">
-  <div class="row">
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-3 col-md-4 col-md-2">
-      <div class="card" style="width: 18rem;">
-        <div class="issue" ><center>이익활동</center></div>
-        <div class="card-body">
-        <img class="card-img-top" src="/../../static/images/placeholder.png" alt="Card image cap">
-          <h5 class="card-title"><b>ESTsoft</b></h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>
-    </div>
 </div><!-- row-->
 </div><!-- card-holder-->
 </div><!-- company-->
 
 
+
+  <div class="company">
+  <div class="company-title">
+    채용
+  </div>
+  <div class="card-holder">
+    <div class="row">
+      <div class="col-md-3 col-md-4 col-md-2">
+        <div class="card" style="width: 18rem;">
+          <div class="issue" ><center>2017년 10월11일 10:56</center></div>
+          <div class="card-body">
+          <img class="card-img-top" src="http://thumb.zumst.com/530x0/http://static.news.zumst.com/images/24/2017/10/11/0f01e2cd2e3b4a8eb6bdbfa8fffdb66f.jpg" alt="Card image cap">
+            <h5 class="card-title"><b>이스트소프트, ‘2017 하반기 공개 채용’… 학력·나이 제한 없어</b></h5>
+            <p class="card-text">종합 소프트웨어 기업 이스트소프트와 자회사 이스트시큐리티, 줌인터넷이 ‘2017년 하반기 공개 채용’을 진행한다. 10월 11일부터 24일까지 공식 홈페이지에서 온라인으로 서류 접수를 진행하며, 최대 40명 내외의 신입 및 경력 인력을 최종 선발할 계획이다. </p>
+          </div>
+        </div>
+      </div>
+  </div><!-- row-->
+</div><!-- card-holder-->
+</div><!-- company-->
 
 
 
